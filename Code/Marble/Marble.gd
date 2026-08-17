a2388531f9b8c5f5393765c26ca4ba69da650862
unblinky <>
2026-08-17 14:41:49 -0700

    Generating rooms in a random sized grid.

--- Code/Marble/Marble.gd
+++ Code/Marble/Marble.gd
@@ -1,13 +1,11 @@
 extends RigidBody3D
 class_name Marble
 
+var starting_position: Vector3
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	starting_position = position
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if position.y < -100:
+		position = starting_position
--- Code/Maze/Maze.gd
+++ Code/Maze/Maze.gd
@@ -1,7 +1,14 @@
 extends Node3D
 class_name Maze
 
+const ROOM = preload("res://Room/Room.tscn")
+
 var speed = 45 # degrees / sec.
+
+func _ready() -> void:
+	var width = randi_range(1, 8)
+	var height = randi_range(1, 8)
+	generate(width, height)
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("quit"):
@@ -17,8 +24,10 @@ func _process(delta: float) -> void:
 		rotation_degrees.z += speed * delta
 
 
-
-
 # Fill he grid with rooms.
-func generate(width, depth): # unit is plot.
-	pass
+func generate(width, height): # unit is plot.
+	for y in height:
+		for x in width:
+			var room: Room = ROOM.instantiate()
+			room.position = Vector3(x, 0, y) - Vector3(width * 0.5 - 0.5, 0, height * 0.5 - 0.5)
+			add_child(room)
