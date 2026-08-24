91ba70f16b76227495b1ef43a0c62c429db2b3a6
unblinky <>
2026-08-24 15:19:24 -0700

    Setting up the visited plots.

--- Code/Maze/Maze.gd
+++ Code/Maze/Maze.gd
@@ -6,8 +6,8 @@ const ROOM = preload("res://Room/Room.tscn")
 var tilt_speed = 60 # degrees / sec.
 
 func _ready() -> void:
-	var width = randi_range(1, 8)
-	var height = randi_range(1, 8)
+	var width: int = 4 # randi_range(1, 8)
+	var height: int = 4 # randi_range(1, 8)
 	generate(width, height)
 
 func _process(delta: float) -> void:
@@ -29,10 +29,19 @@ func generate(width, height): # unit is plot.
 	#var room_count: int = width * height
 	
 	# Plots on our grid.
-	var breadcrumbs: Array[Vector2] = [Vector2(0, 0)]
+	var breadcrumbs: Array[Vector2] = [Vector2(0, 0)] # Array changes size.
+	var visited_plots: Array[Vector2i] # Only grows.
+	
+	var current_plot: Vector2i = Vector2i.ZERO # TODO: Randomize the starting room's plot.
+	
+	
+	# HACK: 
+	var upwards: float = 0.0
+	
 	
 	# 3D Space.
-	var current_position: Vector3 = Vector3.ZERO
+	#var current_position: Vector3 = Vector3.ZERO
+	
 	
 	for y in height:
 		for x in width:
@@ -40,7 +49,24 @@ func generate(width, height): # unit is plot.
 			var room: Room = ROOM.instantiate()
 			add_child(room)
 			
+			
 			# TODO: Recenter the maze
 			#room.position = current_position - Vector3(width * 0.5 - 0.5, 0, height * 0.5 - 0.5)
-			room.position += current_position + room.get_random_direction()
-			current_position = room.position
+			
+			#var next_plot: Vector2i = (Vector2i.RIGHT + current_plot) * room.size
+			var next_plot: Vector2i = (room.get_random_direction() + current_plot) * room.size
+			
+			if not visited_plots.has(next_plot):
+				visited_plots.append(next_plot)
+			#else:
+				
+			
+			room.position.x += next_plot.x
+			room.position.z += next_plot.y
+			
+			# HACK: Just to see the room gen better.
+			upwards += 0.1
+			room.position.y = upwards
+			
+			current_plot = next_plot
+			#current_position = room.position
--- Code/Room/Room.gd
+++ Code/Room/Room.gd
@@ -2,6 +2,7 @@ extends Node3D
 class_name Room
 
 var walls: Array
+var size: Vector2i = Vector2i(1, 1) # Meters. TODO: Support non-uniform room sizes and positional floats.
 
 func _ready() -> void:
 	#walls = [$NorthWall, $SouthWall, $EastWall, $WestWall]
@@ -11,17 +12,21 @@ func _ready() -> void:
 	walls.append($WestWall)
 
 
-func get_random_direction() -> Vector3:
-	var rando: int = randi_range(0 ,3)
+func get_random_direction() -> Vector2i:
+	var rando: int = randi_range(0 ,walls.size() - 1)
 	
 	match walls[rando].direction:
 		Wall.Direction.NORTH:
-			return Vector3.FORWARD
+			return Vector2i.UP
 		Wall.Direction.EAST:
-			return Vector3.RIGHT
+			return Vector2i.RIGHT
 		Wall.Direction.SOUTH:
-			return Vector3.BACK
+			return Vector2i.DOWN
 		Wall.Direction.WEST:
-			return Vector3.LEFT
+			return Vector2i.LEFT
 	
-	return Vector3.ZERO
+	walls.remove_at(rando)
+	queue_free()
+	#walls.erase(walls[rando])
+	
+	return Vector2i.ZERO
